68827dd7b8092e222247cc144053a4da49fd6d4e
Kelsey Crow <kelseybcrow@gmail.com>
2021-11-19 10:22:45 -0700

    finished fortunes button

--- server/index.js
+++ server/index.js
@@ -3,15 +3,15 @@ const cors = require("cors");
 
 const app = express();
 
-
 app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
 
 app.get("/api/compliment", (req, res) => {
-  const compliments = ["Gee, you're a smart cookie!",
-					 "Cool shirt!",
-					 "Your Javascript skills are stellar.",
+  const compliments = [
+    "Gee, you're a smart cookie!",
+    "Cool shirt!",
+    "Your Javascript skills are stellar.",
   ];
 
   // choose random compliment
@@ -19,7 +19,22 @@ app.get("/api/compliment", (req, res) => {
   let randomCompliment = compliments[randomIndex];
 
   res.status(200).send(randomCompliment);
-  
+});
+
+app.get("/api/fortune", (req, res) => {
+  const fortunes = [
+    "You will meet someone important soon.",
+    "Your path will diverge from tradition dramatically.",
+    "You will know the answer to your question soon.",
+    "Your income will be as stable as your emotions are.",
+    "Your purpose will be revealed when you are ready.",
+  ];
+
+  // choose random fortune
+  let randomIndex = Math.floor(Math.random() * fortunes.length);
+  let randomFortune = fortunes[randomIndex];
+
+  res.status(200).send(randomFortunes);
 });
 
 app.listen(4000, () => console.log("Server running on 4000"));
